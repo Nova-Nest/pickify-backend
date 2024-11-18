d0cc265d91836d72a13026f0f52299137b3c1ef3
yesrin <asjs11@naver.com>
2024-11-18 23:12:30 +0900

    프로젝트 초기 설정

---
+++ src/main/java/pickify/pickifybackend/PickifyBackendApplication.java
@@ -0,0 +1,13 @@
+package pickify.pickifybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickifyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickifyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pickify/pickifybackend/PickifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pickify.pickifybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickifyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
