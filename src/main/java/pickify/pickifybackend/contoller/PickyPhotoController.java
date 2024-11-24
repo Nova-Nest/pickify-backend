7ddeb078fb3beaf9d57e5a306ed86b787adab594
rojojun <zoloman316@gmail.com>
2024-11-24 10:54:42 +0900

    [Add] Vertex AI 관련 의존성 추가

--- src/main/java/pickify/pickifybackend/contoller/PickyPhotoController.java
+++ src/main/java/pickify/pickifybackend/contoller/PickyPhotoController.java
@@ -1,4 +1,23 @@
 package pickify.pickifybackend.contoller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import pickify.pickifybackend.dto.PickyPhotoRequest;
+import pickify.pickifybackend.dto.PickyPhotoResponse;
+import pickify.pickifybackend.service.PickyPhotoService;
+
+@RequiredArgsConstructor
+@RestController
 public class PickyPhotoController {
+    private final PickyPhotoService pickyPhotoService;
+
+    @PostMapping("/picky/extract")
+    public ResponseEntity<PickyPhotoResponse> getAIResult(@RequestBody PickyPhotoRequest pickyPhotoRequest) {
+        PickyPhotoResponse result = pickyPhotoService.getAIResult();
+        return ResponseEntity.ok(result);
+    }
 }
--- src/main/java/pickify/pickifybackend/service/PickyPhotoService.java
+++ src/main/java/pickify/pickifybackend/service/PickyPhotoService.java
@@ -1,4 +1,12 @@
 package pickify.pickifybackend.service;
 
+import org.springframework.stereotype.Service;
+import pickify.pickifybackend.dto.PickyPhotoResponse;
+
+@Service
 public class PickyPhotoService {
+
+    public PickyPhotoResponse getAIResult() {
+        return null;
+    }
 }
