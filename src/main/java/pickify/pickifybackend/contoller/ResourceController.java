4329b265c805cabf96a9d52baaebde09512b2005
hobit22 <hobit22@naver.com>
2024-11-27 05:05:56 +0900

    [ADD] gcp signedUrl 추가

--- src/main/java/pickify/pickifybackend/contoller/ResourceController.java
+++ src/main/java/pickify/pickifybackend/contoller/ResourceController.java
@@ -1,11 +1,13 @@
 package pickify.pickifybackend.contoller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import pickify.pickifybackend.service.ResourceService;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,5 +18,13 @@ public class ResourceController {
     @PostMapping("/upload")
     public String upload(@RequestBody MultipartFile file) {
         return resourceService.uploadFile(file);
+    }
+
+    @GetMapping("/signedUrl")
+    public Map<String, URL> getSignedUrl(String contentType, int minutes) {
+        URL signedUrl = resourceService.getSignedUrl(contentType, minutes);
+        Map<String, URL> response = new HashMap<>();
+        response.put("signedUrl", signedUrl);
+        return response;
     }
 }
--- src/main/java/pickify/pickifybackend/service/ResourceService.java
+++ src/main/java/pickify/pickifybackend/service/ResourceService.java
@@ -2,17 +2,24 @@ package pickify.pickifybackend.service;
 
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.HttpMethod;
 import com.google.cloud.storage.Storage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.URL;
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class ResourceService {
 
@@ -50,5 +57,36 @@ public class ResourceService {
             return filename.substring(filename.lastIndexOf("."));
         }
         return ""; // 확장자가 없는 경우 빈 문자열 반환
+    }
+
+    public URL getSignedUrl(String contentType, int minutes) {
+        log.info("get signed url");
+        String uuid = UUID.randomUUID().toString(); // Google Cloud Storage에 저장될 파일 이름
+
+        String fileName = LocalDate.now() + "/" + uuid + ".png"; // 날짜 기반 디렉토리 구조 포함
+
+        log.info("fileName : {}", fileName);
+
+        BlobId blobId = BlobId.of(bucketName, fileName);
+
+        BlobInfo blobInfo = BlobInfo.newBuilder(blobId)
+                .setContentType(contentType)
+                .build();
+
+
+        // Generate Signed URL
+        Map<String, String> extensionHeaders = new HashMap<>();
+        extensionHeaders.put("Content-Type", contentType);
+
+        URL url =
+                storage.signUrl(
+                        blobInfo,
+                        minutes,
+                        TimeUnit.MINUTES,
+                        Storage.SignUrlOption.httpMethod(HttpMethod.PUT),
+                        Storage.SignUrlOption.withExtHeaders(extensionHeaders),
+                        Storage.SignUrlOption.withV4Signature());
+
+        return url;
     }
 }
