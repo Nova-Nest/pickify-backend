8ae831fc5e75d30b993fe5ceaa729edee01688d7
hobit22 <hobit22@naver.com>
2024-11-27 03:14:48 +0900

    [ADD] upload 로직 수정

--- src/main/java/pickify/pickifybackend/service/ResourceService.java
+++ src/main/java/pickify/pickifybackend/service/ResourceService.java
@@ -1,8 +1,7 @@
 package pickify.pickifybackend.service;
 
-
+import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
-import com.google.cloud.storage.BucketInfo;
 import com.google.cloud.storage.Storage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Service
@@ -24,20 +24,31 @@ public class ResourceService {
     public String uploadFile(MultipartFile file) {
 
         String uuid = UUID.randomUUID().toString(); // Google Cloud Storage에 저장될 파일 이름
-        String ext = file.getContentType(); // 파일의 형식 ex) JPG
-
-
+        String contentType = file.getContentType(); // 파일의 형식 ex) JPG
+        String extension = getExtension(file.getOriginalFilename()); // 확장자 추출
+        String fileName = LocalDate.now() + "/" + uuid + extension; // 날짜 기반 디렉토리 구조 포함
         // Cloud에 이미지 업로드
         try {
-            BlobInfo blobInfo = storage.create(
-                    BlobInfo.newBuilder(bucketName, uuid)
-                            .setContentType(ext)
-                            .build(),
-                    file.getInputStream()
-            );
+            BlobId blobId = BlobId.of(bucketName, fileName);
+
+            // BlobInfo 생성 (파일 정보)
+            BlobInfo blobInfo = BlobInfo.newBuilder(blobId)
+                    .setContentType(contentType) // 파일 형식 설정
+                    .build();
+
+            storage.create(blobInfo, file.getInputStream());
+
             return blobInfo.getMediaLink();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+
+    private String getExtension(String filename) {
+        if (filename != null && filename.contains(".")) {
+            return filename.substring(filename.lastIndexOf("."));
+        }
+        return ""; // 확장자가 없는 경우 빈 문자열 반환
     }
 }
