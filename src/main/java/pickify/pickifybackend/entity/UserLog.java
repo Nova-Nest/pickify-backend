83675fa4732cdcc9a89e5f6cc5358d23b1966eab
rojojun <zoloman316@gmail.com>
2024-11-29 21:23:44 +0900

    [Edit] Add CreatedAt field and orderBy query

--- src/main/java/pickify/pickifybackend/entity/UserLog.java
+++ src/main/java/pickify/pickifybackend/entity/UserLog.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +14,7 @@ import java.util.List;
 public class UserLog {
     @Id
     private String id;
+    private LocalDateTime createdAt;
     private String userUuid;
     private String mainKeyword;
     private List<String> builtInAiKeywords; //나영님이 주시는 Keywords
@@ -24,6 +26,7 @@ public class UserLog {
     @Builder
     private UserLog(String userUuid, String mainKeyword, List<String> builtInAiKeywords, List<String> geminiReturnKeywords,
                     String category, String originalImageUrl, List<String> resultImageUrls) {
+        this.createdAt = LocalDateTime.now();
         this.userUuid = userUuid;
         this.mainKeyword = mainKeyword;
         this.builtInAiKeywords = builtInAiKeywords;
--- src/main/java/pickify/pickifybackend/repository/UserLogRepository.java
+++ src/main/java/pickify/pickifybackend/repository/UserLogRepository.java
@@ -6,5 +6,5 @@ import pickify.pickifybackend.entity.UserLog;
 import java.util.List;
 
 public interface UserLogRepository extends MongoRepository<UserLog, String> {
-    List<UserLog> findAllByCategoryAndUserUuid(String category, String userUuid);
+    List<UserLog> findAllByCategoryAndUserUuidOrderByCreatedAtDesc(String category, String userUuid);
 }
